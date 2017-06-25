874dbb90f4d2ae657f57b65c6769ffdbd83da58d
Claire Furney <infuerno@fastmail.fm>
2017-06-25 11:10:39 +0100

    Finish part 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,28 +6,36 @@ class MoviesController < ApplicationController
   end
 
   def show
-    id = params[:id] # retrieve movie ID from URI route
-    @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
+    @movie = Movie.find(params[:id])
   end
 
   def index
-    if params['ratings']
-      @movies = Movie.where({:rating => params['ratings'].keys })
-      @selected_ratings = params['ratings']
+    # check if any keys in the session are not in the params, need to be to be RESTful
+    new_params = Hash.new
+    [:ratings, :order].each do | k | 
+      if session[k] and !params[k]
+        new_params[k] = session[k]
+      end
+    end
+    if new_params.length > 0
+      logger.debug "Params in session but not in URI, redirecting to correct URI (RESTful)"
+      logger.debug "New params: #{new_params}"
+      redirect_to movies_path(new_params.merge!(params))
+    end
+
+    if params[:ratings]
+      @movies = Movie.where({:rating => params[:ratings].keys })
+      @selected_ratings = params[:ratings]
+      session[:ratings] = params[:ratings]
     else
       @movies = Movie.all
       @selected_ratings = Movie.all_ratings
     end
 
-    # only title and release_date are valid for sorting
-    # need to do explicity or ensure we restrict it
-    if params[:order] == "title"
-      @movies.order! :title
-      @order = :title
-    elsif params[:order] == "release_date"
-      @movies.order! :release_date        
-      @order = :release_date
+    if ["title", "release_date"].include? params[:order]
+      @movies.order! params[:order]
+      @order = params[:order]
+      session[:order] = params[:order]
     end
   end
 
